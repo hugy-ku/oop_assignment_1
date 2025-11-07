ca041ceb7d815649f1098f6bafde5f57bcc43f68
Hugh Jaiden <hugh.j@ku.th>
2025-11-07 14:21:17 +0700

    Create Library class and added tests for Book and Library

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -7,3 +7,21 @@ class Book():
         self.author = author
         self.available_copies = copies
         self.total_copies = copies
+
+class Library():
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book_id, title, author, copies):
+        book = Book(book_id, title, author, copies)
+        self.books.append(book)
+        return f"Book '{title}' added successfully!"
+    
+    def display_available_books(self):
+        books = self.books
+        result = []
+        result.append("\n=== Available Books ===")
+        for book in books:
+            if book.available_copies > 0:
+                result.append(f"{book.title} by {book.author} - {book.available_copies} copies available")
+        return '\n'.join(result)
