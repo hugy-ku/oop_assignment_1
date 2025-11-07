2f6b79a03526803523463b99cf3c15f1aa371d46
Hugh Jaiden <hugh.j@ku.th>
2025-11-07 14:05:01 +0700

    Create Book class

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -1,1 +1,9 @@
-# placeholder
+# Library Management System - OOP Style
+
+class Book():
+    def __init__(self, book_id, title, author, copies):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.available_copies = copies
+        self.total_copies = copies
